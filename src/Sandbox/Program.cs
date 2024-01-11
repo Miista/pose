81df5e147e0b28123b405793b2e03d86efe46b53
Søren Guldmund <sgu@immeo.dk>
2024-01-11 13:52:54 +0100

    Add sandbox

---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Pose;
+
+namespace Pose.Sandbox
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shim dateTimeShim = Shim.Replace(() => T.I).With(() => "L");
+            Shim dateTimeShim1 = Shim.Replace(() => T.Get()).With(() => "Word");
+            var inst = new Inst();
+            Func<Inst, string> f = new Func<Inst, string>(i => "Word");
+            Shim dateTimeShim2 = Shim.Replace(() => inst.S).With(f);
+            Shim dateTimeShim3 = Shim.Replace(() => inst.Get()).With(f);
+            Shim dateTimeShim4 = Shim.Replace(() => Is.A<Inst>().S).With(f);
+            Shim dateTimeShim5 = Shim.Replace(() => Is.A<Inst>().Get()).With(f);
+            Shim dateTimeShim6 = Shim.Replace(() => Is.A<Inst>().Get()).With(delegate(Inst @this) { return "Word"; });
+            
+            PoseContext.Isolate(
+                () =>
+                {
+                    // Console.Write(T.I);
+                    // Console.WriteLine(T.Get());
+                    // Console.WriteLine(inst.S);
+                    Console.WriteLine(inst.Get());
+                }, dateTimeShim1, dateTimeShim2, dateTimeShim3, dateTimeShim4, dateTimeShim5, dateTimeShim6);
+        }
+    }
+
+    public class Inst
+    {
+        public string S { get; set; }
+
+        public string Get()
+        {
+            return string.Empty;
+        }
+    }
+    
+    public static class T
+    {
+        public static string I
+        {
+            get { return "H"; }
+        }
+
+        public static string Get() => "Hello";
+    }
+}
