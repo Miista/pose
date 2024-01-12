f1d2803c0aa693fdce78e66816824858e7e68000
Søren Guldmund <sgu@immeo.dk>
2024-01-12 10:03:06 +0100

    #4 Rewrite more tests

--- test/Pose.Tests/IL/MethodRewriterTests.cs
+++ test/Pose.Tests/IL/MethodRewriterTests.cs
@@ -1,17 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-
+using FluentAssertions;
 using Pose.Helpers;
 using Pose.IL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Pose.Tests
 {
-    [TestClass]
     public class MethodRewriterTests
     {
+        [Fact]
+        public void Can_rewrite_static_method()
+        {
+            // Arrange
+            var methodInfo = typeof(DateTime).GetMethod("get_Now");
+            var methodRewriter = MethodRewriter.CreateRewriter(methodInfo, false);
+
+            // Act
+            var dynamicMethod = methodRewriter.Rewrite() as DynamicMethod;
+            var func = dynamicMethod.CreateDelegate(typeof(Func<DateTime>));
+
+            // Assert
+            Assert.AreEqual(DateTime.Now.ToString("yyyyMMdd_HHmm"), ((DateTime)func.DynamicInvoke()).ToString("yyyyMMdd_HHmm"));
+        }
+        
         [TestMethod]
         public void TestStaticMethodRewrite()
         {
@@ -23,32 +40,43 @@ namespace Pose.Tests
             Assert.AreEqual(DateTime.Now.ToString("yyyyMMdd_HHmm"), ((DateTime)func.DynamicInvoke()).ToString("yyyyMMdd_HHmm"));
         }
 
-        [TestMethod]
-        public void TestInstanceMethodRewrite()
+        [Fact]
+        public void Can_rewrite_instance_method()
         {
-            string item = "Item 1";
-            List<string> list = new List<string>();
-            MethodInfo methodInfo = typeof(List<string>).GetMethod("Add");
-            MethodRewriter methodRewriter = MethodRewriter.CreateRewriter(methodInfo, false);
-            DynamicMethod dynamicMethod = methodRewriter.Rewrite() as DynamicMethod;
+            // Arrange
+            const string item = "Item 1";
+            
+            var list = new List<string>();
+            var methodInfo = typeof(List<string>).GetMethod(nameof(List<string>.Add));
+            var methodRewriter = MethodRewriter.CreateRewriter(methodInfo, false);
+            var dynamicMethod = methodRewriter.Rewrite() as DynamicMethod;
+            var func = dynamicMethod.CreateDelegate(typeof(Action<List<string>, string>));
 
-            Delegate func = dynamicMethod.CreateDelegate(typeof(Action<List<string>, string>));
+            // Act
             func.DynamicInvoke(list, item);
 
-            Assert.AreEqual(1, list.Count);
-            Assert.AreEqual(item, list[0]);
+            // Assert
+            list.Should().HaveCount(1);
+            list[0].Should().BeEquivalentTo(item);
         }
 
-        [TestMethod]
-        public void TestConstructorRewrite()
+        [Fact]
+        public void Can_rewrite_constructor()
         {
-            List<string> list = new List<string>();
-            ConstructorInfo constructorInfo = typeof(List<string>).GetConstructor(Type.EmptyTypes);
-            MethodRewriter methodRewriter = MethodRewriter.CreateRewriter(constructorInfo, false);
-            DynamicMethod dynamicMethod = methodRewriter.Rewrite() as DynamicMethod;
+            // Arrange
+            var constructorInfo = typeof(List<string>).GetConstructor(Type.EmptyTypes);
+            var methodRewriter = MethodRewriter.CreateRewriter(constructorInfo, false);
+            
+            // Act
+            var dynamicMethod = methodRewriter.Rewrite() as DynamicMethod;
 
-            Assert.AreEqual(typeof(void), dynamicMethod.ReturnType);
-            Assert.AreEqual(typeof(List<string>), dynamicMethod.GetParameters()[0].ParameterType);
+            // Assert
+            dynamicMethod.Should().NotBeNull();
+            dynamicMethod.ReturnType.Should().Be(typeof(void));
+            
+            var firstParameter = dynamicMethod.GetParameters().FirstOrDefault();
+            firstParameter.Should().NotBeNull();
+            firstParameter.ParameterType.Should().Be<List<string>>(because: "that is the first parameter to the constructor");
         }
     }
 }
--- test/Pose.Tests/IL/StubsTests.cs
+++ test/Pose.Tests/IL/StubsTests.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-
+using FluentAssertions;
 using Pose.Helpers;
 using Pose.IL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -22,6 +23,25 @@ namespace Pose.Tests
             Assert.AreEqual(methodInfo.GetParameters().Length, dynamicMethod.GetParameters().Length);
             Assert.AreEqual(methodInfo.GetParameters()[0].ParameterType, dynamicMethod.GetParameters()[0].ParameterType);
         }
+        
+        [TestMethod]
+        public void Can_generate_stub_for_static_method()
+        {
+            // Arrange
+            var methodInfo = typeof(Console).GetMethod(nameof(Console.WriteLine), new[] { typeof(string) });
+            
+            // Act
+            var dynamicMethod = Stubs.GenerateStubForDirectCall(methodInfo);
+            
+            // Assert
+            var dynamicParameters = dynamicMethod.GetParameters();
+            var methodParameters = methodInfo.GetParameters();
+            dynamicParameters.Should().HaveSameCount(methodParameters);
+            
+            var firstDynamicParameter = dynamicParameters.FirstOrDefault()?.ParameterType;
+            var firstMethodParameter = methodParameters.FirstOrDefault()?.ParameterType;
+            firstDynamicParameter.Should().Be(firstMethodParameter);
+        }
 
         [TestMethod]
         public void TestGenerateStubForInstanceMethod()
@@ -33,6 +53,27 @@ namespace Pose.Tests
 
             Assert.AreEqual(methodInfo.GetParameters().Length, dynamicMethod.GetParameters().Length - 1);
             Assert.AreEqual(thisType, dynamicMethod.GetParameters()[0].ParameterType);
+        }
+        
+        [TestMethod]
+        public void Can_generate_stub_for_instance_method()
+        {
+            // Arrange
+            var thisType = typeof(List<string>);
+            var methodInfo = thisType.GetMethod(nameof(List<string>.Add));
+            
+            // Act
+            var dynamicMethod = Stubs.GenerateStubForDirectCall(methodInfo);
+
+            // Assert
+            var dynamicParameters = dynamicMethod.GetParameters();
+            dynamicParameters.Should().HaveCount(2, because: "the dynamic method takes both the instance parameter and the value to be added");
+
+            var instanceParameter = dynamicParameters[0];
+            instanceParameter.ParameterType.Should().Be(thisType, because: "the first parameter is the instance");
+            
+            var valueParameter = dynamicParameters[1];
+            valueParameter.ParameterType.Should().Be(typeof(string), because: "the second parameter is the value to be added");
         }
 
         [TestMethod]
@@ -46,6 +87,27 @@ namespace Pose.Tests
             Assert.AreEqual(methodInfo.GetParameters().Length, dynamicMethod.GetParameters().Length - 1);
             Assert.AreEqual(thisType, dynamicMethod.GetParameters()[0].ParameterType);
         }
+        
+        [TestMethod]
+        public void Can_generate_stub_for_virtual_call()
+        {
+            // Arrange
+            var thisType = typeof(List<string>);
+            var methodInfo = thisType.GetMethod(nameof(List<string>.Add));
+            
+            // Act
+            var dynamicMethod = Stubs.GenerateStubForVirtualCall(methodInfo);
+            
+            // Assert
+            var dynamicParameters = dynamicMethod.GetParameters();
+            dynamicParameters.Should().HaveCount(2, because: "the dynamic method takes both the instance parameter and the value to be added");
+
+            var instanceParameter = dynamicParameters[0];
+            instanceParameter.ParameterType.Should().Be(thisType, because: "the first parameter is the instance");
+            
+            var valueParameter = dynamicParameters[1];
+            valueParameter.ParameterType.Should().Be(typeof(string), because: "the second parameter is the value to be added");
+        }
 
         [TestMethod]
         public void TestGenerateStubForReferenceTypeConstructor()
@@ -58,16 +120,45 @@ namespace Pose.Tests
             Assert.AreEqual(constructorInfo.GetParameters().Length, dynamicMethod.GetParameters().Length);
             Assert.AreEqual(thisType, dynamicMethod.ReturnType);
         }
+        
+        [TestMethod]
+        public void Can_generate_stub_for_reference_type_constructor()
+        {
+            // Arrange
+            var thisType = typeof(List<string>);
+            var constructorInfo = thisType.GetConstructor(Type.EmptyTypes);
+            
+            // Act
+            var dynamicMethod = Stubs.GenerateStubForObjectInitialization(constructorInfo);
+            
+            // Assert
+            constructorInfo.GetParameters().Should().HaveSameCount(dynamicMethod.GetParameters());
+            dynamicMethod.ReturnType.Should().Be(thisType);
+        }
 
         [TestMethod]
         public void TestGenerateStubForMethodPointer()
         {
-            MethodInfo methodInfo = typeof(Console).GetMethod("WriteLine", new[] { typeof(string) });
+            MethodInfo methodInfo = typeof(Console).GetMethod(nameof(Console.WriteLine), new[] { typeof(string) });
             DynamicMethod dynamicMethod = Stubs.GenerateStubForDirectLoad(methodInfo);
             int count = dynamicMethod.GetParameters().Length;
 
             Assert.AreEqual(0, dynamicMethod.GetParameters().Length);
             Assert.AreEqual(typeof(IntPtr), dynamicMethod.ReturnType);
+        }
+        
+        [TestMethod]
+        public void Can_generate_stub_for_method_pointer()
+        {
+            // Arrange
+            var methodInfo = typeof(Console).GetMethod(nameof(Console.WriteLine), new[] { typeof(string) });
+            
+            // Act
+            var dynamicMethod = Stubs.GenerateStubForDirectLoad(methodInfo);
+            
+            // Assert
+            dynamicMethod.GetParameters().Should().HaveCount(0);
+            dynamicMethod.ReturnType.Should().Be<IntPtr>();
         }
     }
 }
