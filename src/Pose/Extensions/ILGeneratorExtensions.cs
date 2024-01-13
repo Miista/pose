55a8d06ccc5585dd48a7ddeaa8dbb431fc3baf89
Søren Guldmund <sgu@immeo.dk>
2024-01-13 14:08:02 +0100

    #8 Locate BakeByteArray method
    In .NET 8 the method has been moved to RuntimeILGenerator which is an internal class.
    Thus the unsafe lookup of the type.
    
    Please note that we still suffer from the same issue as described in #6.
    
    Closes #8

--- src/Pose/Extensions/ILGeneratorExtensions.cs
+++ src/Pose/Extensions/ILGeneratorExtensions.cs
@@ -8,10 +8,18 @@ namespace Pose.Extensions
     {
         public static byte[] GetILBytes(this ILGenerator ilGenerator)
         {
+#if NET8_0_OR_GREATER
+            var runtimeILGeneratorType = Type.GetType("System.Reflection.Emit.RuntimeILGenerator") ?? throw new Exception("Cannot find type System.Reflection.Emit.RuntimeILGenerator");
+            var bakeByteArray = runtimeILGeneratorType.GetMethod("BakeByteArray", BindingFlags.Instance | BindingFlags.NonPublic)
+                                ?? throw new Exception($"Cannot get method BakeByteArray from type {nameof(ILGenerator)}");
+            var ilBytes = (byte[])bakeByteArray.Invoke(ilGenerator, null);
+            return ilBytes;
+#else
             var bakeByteArray = typeof(ILGenerator).GetMethod("BakeByteArray", BindingFlags.Instance | BindingFlags.NonPublic)
                                 ?? throw new Exception($"Cannot get method BakeByteArray from type {nameof(ILGenerator)}");
             var ilBytes = (byte[])bakeByteArray.Invoke(ilGenerator, null);
             return ilBytes;
+#endif
         }
     }
 }
