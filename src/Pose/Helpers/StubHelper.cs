92222c2231cf1fd40499b5375c21d87a386f2879
Søren Guldmund <sgu@immeo.dk>
2024-01-29 08:51:25 +0100

    Remove redundant platform override #if

--- src/Pose/Helpers/StubHelper.cs
+++ src/Pose/Helpers/StubHelper.cs
@@ -112,11 +112,7 @@ namespace Pose.Helpers
                 if (genericArguments.Length > 0)
                 {
                     name += "[";
-#if NETSTANDARD2_1_OR_GREATER
-                    name += string.Join(',', genericArguments.Select(g => g.Name));
-#else
                     name += string.Join(",", genericArguments.Select(g => g.Name));
-#endif
                     name += "]";
                 }
             }
