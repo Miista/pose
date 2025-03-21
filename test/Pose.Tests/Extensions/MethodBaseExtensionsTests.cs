b2effcceaa78c1662a2effd97543d4a380c23292
Søren Guldmund <palmund@gmail.com>
2025-03-21 09:44:46 +0100

    Add negative test for InCoreLibrary

--- test/Pose.Tests/Extensions/MethodBaseExtensionsTests.cs
+++ test/Pose.Tests/Extensions/MethodBaseExtensionsTests.cs
@@ -19,5 +19,23 @@ namespace Pose.Tests
             // Assert
             result.Should().BeTrue(because: $"{nameof(Exception.ToString)} is in the core library");
         }
+
+        private class NotInCoreLibrary
+        {
+            public override string ToString() => "This is a string representation";
+        }
+        
+        [Fact]
+        public void Not_InCore_library()
+        {
+            // Arrange
+            var sut = typeof(NotInCoreLibrary).GetMethod(nameof(NotInCoreLibrary.ToString), Type.EmptyTypes);
+
+            // Act
+            var result = sut.InCoreLibrary();
+
+            // Assert
+            result.Should().BeFalse(because: $"{nameof(NotInCoreLibrary.ToString)} is not in the core library");
+        }
     }
 }
